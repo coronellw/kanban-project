e0fd310cb18cacdfeae2222bc51bf1351a222cee
Wiston Coronell <coronell@gmail.com>
2024-12-16 10:48:02 -0400

    adding button element �

---
+++ app/ui/button/button.tsx
@@ -0,0 +1,32 @@
+import type React from "react"
+import styles from "./button.module.css"
+import classNames from "classnames"
+
+type Button = {
+  btnSize?: "large" | "small"
+  btnType?: "primary" | "secondary" | "destructive"
+} & React.ComponentPropsWithoutRef<'button'>
+
+const Button = ({ children, disabled, btnType = "primary", btnSize = "large", ...props }: Button) => {
+
+  return (
+    <button
+      className={classNames(
+        {
+          [styles.primary]: !disabled && btnType === "primary",
+          [styles.secondary]: !disabled && btnType === "secondary",
+          [styles.destructive]: !disabled && btnType === "destructive",
+          [styles.normal]: !disabled && btnSize === "large",
+          [styles.small]: !disabled && btnSize === "small",
+          [styles.disabled]: disabled,
+        }
+      )}
+      disabled={disabled}
+      {...props}
+    >
+      {children}
+    </button >
+  )
+}
+
+export default Button
