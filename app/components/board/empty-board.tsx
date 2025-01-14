abe124a0f019ee05e669098e5ebad090015c0641
Wiston Coronell <coronell@gmail.com>
2025-01-14 00:40:15 -0400

    getting closer to pixel perfect

--- app/components/board/empty-board.tsx
+++ app/components/board/empty-board.tsx
@@ -7,7 +7,7 @@ import { ModalWindows } from "~/types"
 const EmptyBoard = () => {
   const setModal = useSetAtom(activeModalAtom)
   return <div className="emptyBoard">
-    <p className="text-heading-l text-center">This board is empty. Create a new column to get started</p>
+    <p className="text-heading-l text-center">This board is empty. Create a new column to get started.</p>
     <Button onClick={() => setModal(ModalWindows.EditBoard)} className="self-center">+ Add New Column</Button>
   </div>
 }
