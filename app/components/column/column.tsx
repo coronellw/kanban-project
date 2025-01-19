aad1f61d9164ec6968c6ddeb71dd27348a3dbba6
Wiston Coronell <coronell@gmail.com>
2025-01-18 22:38:36 -0400

    fixing column styles

--- app/components/column/column.tsx
+++ app/components/column/column.tsx
@@ -6,8 +6,8 @@ import styles from "./column.module.css"
 import classNames from "classnames"
 
 const colors = [
-  "#8471F2",
   "#49C4E5",
+  "#8471F2",
   "#67E2AE"
 ]
 
@@ -23,7 +23,7 @@ const Column = ({ column, index }: { column: IColumn, index: number }) => {
       >
         <span className={styles.columnHeader}>
           <span className={styles.columnColorCircle} style={style}></span>
-          <h2 className="text-heading-s capitalize">{column.name} ({column.tasks.length})</h2>
+          <h2>{column.name} ({column.tasks.length})</h2>
         </span>
         {column.tasks.map(task => <Task key={task._id} task={task} />)}
       </div>
