de312b37ac479281efd8f29a45e5fc845d836fa6
Wiston Coronell <coronell@gmail.com>
2025-02-03 23:26:08 -0400

    testing lazy loading to optimally load content

--- app/elements/backdrop/backdrop.tsx
+++ app/elements/backdrop/backdrop.tsx
@@ -1,9 +1,15 @@
-import { useEffect, useRef } from "react"
+import { lazy, Suspense, useEffect, useRef } from "react"
 import { useAtom } from "jotai"
 import styles from "./backdrop.module.css"
 import { activeModalAtom } from "~/store"
-import { SidebarModal, AddNewTaskModal, ViewTaskModal, BoardModal, ConfirmTaskDeletionModal, ConfirmBoardDeletion } from "~/components/modals"
 import { ModalWindows } from "~/types"
+
+const BoardModal = lazy(() => import("~/components/modals/board-modal"))
+const SidebarModal = lazy(() => import("~/components/modals/side-bar-modal"))
+const ViewTaskModal = lazy(() => import("~/components/modals/view-task-modal"))
+const AddNewTaskModal = lazy(() => import("~/components/modals/add-new-task-modal"))
+const ConfirmBoardDeletion = lazy(() => import("~/components/modals/confirm-board-deletion"))
+const ConfirmTaskDeletionModal = lazy(() => import("~/components/modals/confirm-task-deletion"))
 
 const Backdrop = () => {
   const [activeModal, setActiveModal] = useAtom(activeModalAtom)
@@ -32,13 +38,15 @@ const Backdrop = () => {
   }
 
   return <div className={styles.backdrop} aria-labelledby="modal-title" role="dialog" aria-modal="true" ref={backdropRef}>
-    {ModalWindows.Sidebar === activeModal && <SidebarModal />}
-    {ModalWindows.AddNewTask === activeModal && <AddNewTaskModal />}
-    {ModalWindows.ViewTask === activeModal && <ViewTaskModal />}
-    {ModalWindows.AddNewBoard === activeModal && <BoardModal isNew />}
-    {ModalWindows.EditBoard === activeModal && <BoardModal />}
-    {ModalWindows.ConfirmTaskDeletion === activeModal && <ConfirmTaskDeletionModal />}
-    {ModalWindows.confirmBoardDeletion === activeModal && <ConfirmBoardDeletion />}
+    <Suspense fallback={"Loading..."}>
+      {ModalWindows.Sidebar === activeModal && <SidebarModal />}
+      {ModalWindows.AddNewTask === activeModal && <AddNewTaskModal />}
+      {ModalWindows.ViewTask === activeModal && <ViewTaskModal />}
+      {ModalWindows.AddNewBoard === activeModal && <BoardModal isNew />}
+      {ModalWindows.EditBoard === activeModal && <BoardModal />}
+      {ModalWindows.ConfirmTaskDeletion === activeModal && <ConfirmTaskDeletionModal />}
+      {ModalWindows.confirmBoardDeletion === activeModal && <ConfirmBoardDeletion />}
+    </Suspense>
   </div>
 }
 
